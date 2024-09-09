0428a7dfeb52974506a2c6aa733eb5079464df8c
qwertpi <qwertpi@users.noreply.github.com>
2024-09-09 18:47:31 +0100

    feat: Demo http4s

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,30 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import cats.effect.{IO, IOApp}
+import com.comcast.ip4s.{ipv4, port}
+import fs2.io.net.Network
+import org.http4s.HttpRoutes
+import org.http4s.Status
+import org.http4s.dsl.io._
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.server.middleware.Logger
 
-def msg = "I was compiled by Scala 3. :)"
+object Logic:
+	def sayHello(name: String): String = "Hi there " + name
+
+object Routes:
+	val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
+		case GET -> Root / "hello" / name => Status.Ok(Logic.sayHello(name))
+	}
+
+object Main extends IOApp.Simple:
+	// This is where we will put CORS
+	val httpApp = Logger.httpApp(true, true)(Routes.routes.orNotFound)
+
+	val server = for {
+		_ <-
+			EmberServerBuilder.default[IO]
+				.withHost(ipv4"0.0.0.0")
+				.withPort(port"8080")
+				.withHttpApp(httpApp)
+				.build
+	} yield ()
+	val run = server.useForever
