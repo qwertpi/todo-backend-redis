2a79021303bed3d3f805dc8c120468d4a2f47399
qwertpi <qwertpi@users.noreply.github.com>
2024-09-10 12:25:40 +0100

    feat: Add redirect from /ping to /ping/

--- src/main/scala/Routes.scala
+++ src/main/scala/Routes.scala
@@ -2,12 +2,14 @@ import cats.effect.IO
 import org.http4s.HttpRoutes
 import org.http4s.Status
 import org.http4s.dsl.io._
+import org.http4s.headers.Location
+import org.http4s.implicits.uri
 
 object Routes:
 	val routes = HttpRoutes.of[IO] {
 		case DELETE -> Root => Status.Ok(Logic.delAllToDos())
-		case GET -> Root / "ping" => Status.Ok(Logic.redisPing(""))
 		case PUT -> Root => Status.Ok(Logic.addToDo())
 		case GET -> Root => Status.Ok(Logic.getAllToDos())
+		case GET -> Root / "ping" => Status.PermanentRedirect(Location(uri"/ping/"))
 		case GET -> Root / "ping" / msg   => Status.Ok(Logic.redisPing(msg))
 	}
