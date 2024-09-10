efef383763ef044a78e5e28e6fc89d5ff6295106
qwertpi <qwertpi@users.noreply.github.com>
2024-09-10 15:08:02 +0100

    feat: Log errors

--- build.sbt
+++ build.sbt
@@ -1,12 +1,15 @@
 name := "todo-backend-redis"
 scalaVersion := "3.5.0"
+Compile / run / fork := true
 
 val http4sVersion = "0.23.27"
 val jedisVersion = "5.1.5"
+val logbackVersion = "1.5.8"
 val munitVersion = "1.0.1"
 val sttpVersion = "3.9.8"
 val circeVersion = "0.14.10"
 libraryDependencies ++= Seq(
+  "ch.qos.logback" % "logback-classic" % logbackVersion,
   "com.softwaremill.sttp.client3" %% "core" % sttpVersion,
   "io.circe" %% "circe-generic" % circeVersion,
   "org.http4s" %% "http4s-circe" % http4sVersion,
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,10 +2,21 @@ import cats.effect.{IO, IOApp}
 import com.comcast.ip4s.{ipv4, port}
 import fs2.io.net.Network
 import org.http4s.ember.server.EmberServerBuilder
-import org.http4s.server.middleware.{CORS, Logger}
+import org.http4s.Request
+import org.http4s.server.middleware.{CORS, ErrorAction}
 
 object Main extends IOApp.Simple:
-	val httpApp = CORS.policy.withAllowOriginAll(Routes.routes.orNotFound)
+	def logRequest(req: Request[IO]): IO[Unit] =
+		IO.println(s"Request: ${req.method} ${req.uri}")
+		*>
+		req.bodyText.compile.string.flatMap(body => IO.println(s"Body: $body"))
+	def logErrorMessage(err: Throwable): IO[Unit] =
+		IO.println(s"Error: ${err.getMessage()}")
+	def logError(req: Request[IO], err: Throwable): IO[Unit] =
+		logRequest(req) *> logErrorMessage(err)
+
+	val httpApp = CORS.policy.withAllowOriginAll(
+		ErrorAction.httpRoutes[IO](Routes.routes, logError).orNotFound)
 
 	val server = for {
 		_ <-
